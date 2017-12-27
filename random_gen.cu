af8bffe0e69e18be53ec662381e5259c07d2a822
Yan <KevinYan37@163.com>
2017-12-27 10:24:35 +0800

    fix an error and update the code

--- random_gen.cu
+++ random_gen.cu
@@ -6,31 +6,31 @@
 #include <time.h>
 
 
-__global__ void kernel_set_random(curandState *curand_states,int size,long clock_for_rand)
+__global__ void kernel_set_random(curandState *curand_states,int width,int height,long clock_for_rand)
 {
     int x = threadIdx.x + blockIdx.x*blockDim.x;
-    
-    if(x<0 || x>size)
+
+    if(x<0 || x>width)
     {
         return;
     }
-
     curand_init(clock_for_rand,x,0,&curand_states[x]);
 }
 
-__global__ void kernel_random(float *dev_random_array,int height,int width,curandState *curand_states)
+__global__ void kernel_random(float *dev_random_array,int width,int height,curandState *curand_states)
 {
     int x = threadIdx.x + blockIdx.x*blockDim.x;
-    int y = threadIdx.y + blockIdx.y*blockDim.y;
 
-    if(x<0 || x>width || y<0 || y>height)
+    if(x<0 || x>width)
     {
         return;
     }
 
-    int pos = y*width + x;
-
-    dev_random_array[pos] = abs(curand_uniform(&curand_states[x]));
+    for(int y=0;y<height;y++)
+    {
+        int pos = y*width + x;
+        dev_random_array[pos] = abs(curand_uniform(curand_states+x));
+    }
 }
 
 
@@ -62,10 +62,10 @@ int main()
     cuda_status = cudaMalloc((void**)&dev_random_array,sizeof(float)*array_size_width*array_size_height);
     if(cuda_status != cudaSuccess)
     {
-        fprintf(stderr,"dev_reference_image cudaMalloc Failed");
+        fprintf(stderr,"dev_random_array cudaMalloc Failed");
         exit( EXIT_FAILURE );
     }
-    cuda_status = cudaMalloc((void **)&dev_states,sizeof(curandState)*array_size_width);
+    cuda_status = cudaMalloc((void **)&dev_states,sizeof(curandState)*array_size_width*array_size_height);
     if(cuda_status != cudaSuccess)
     {
         fprintf(stderr,"dev_states cudaMalloc Failed");
@@ -74,12 +74,13 @@ int main()
 
     long clock_for_rand = clock();
 
-    dim3 threads_x(16,1);
-    dim3 grid_x(max((array_size_width+threads_x.x-1)/threads_x.x,1),1);
-    kernel_set_random<<<grid_x,threads_x>>>(dev_states,array_size_width,clock_for_rand);
 
-    dim3 threads(16,16);
-    dim3 grid(max((array_size_width+threads.x-1)/threads.x,1),max((array_size_height+threads.y-1)/threads.y,1));  
+    
+
+    dim3 threads(16,1);
+    dim3 grid((array_size_width+threads.x-1)/threads.x,1);  
+
+    kernel_set_random<<<grid,threads>>>(dev_states,array_size_width,array_size_height,clock_for_rand);
     kernel_random<<<grid,threads>>>(dev_random_array,array_size_width,array_size_height,dev_states);
 
     //copy out the result
@@ -92,7 +93,7 @@ int main()
 
     for(int i=0;i<array_size_width*array_size_height;i++)
     {
-        printf("%f\n",random_array[i]);
+        //printf("%f\n",random_array[i]);
     }
 
     //free
